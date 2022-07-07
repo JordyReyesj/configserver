02768f3e95b2991ab61f577e955197eda42554a1
jordyreyesj <jordy.2006.13@gmail.com>
2022-07-07 10:45:55 -0500

    final

--- src/main/java/com/nttdata/configserver/ConfigserverApplication.java
+++ src/main/java/com/nttdata/configserver/ConfigserverApplication.java
@@ -3,7 +3,9 @@ package com.nttdata.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
+/**
+ * Config server.
+ */
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigserverApplication {
